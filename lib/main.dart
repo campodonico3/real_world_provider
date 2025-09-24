e0348d7fedfc81c770727b412f5d4a97817c02a7
Kevin Gege <kcampodonico@unprg.edu.pe>
2025-09-23 22:49:49 -0500

    feat: Integrate Firebase with configuration files and update Gradle settings

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,17 @@ import 'package:real_world_provider/providers/cart_provider.dart';
 import 'package:real_world_provider/providers/discount_provider.dart';
 import 'package:real_world_provider/providers/product_provider.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'core/router/router.dart';
 import 'core/theme/app_colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
